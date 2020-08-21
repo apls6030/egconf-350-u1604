72c184fa268d1e9b8c6dcc942a8b5488414ddaa1
apls6030 <69598976+apls6030@users.noreply.github.com>
2020-08-21 15:38:01 -0500

    Create register.sh

---
+++ register.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ejabberdctl register router private.localhost xmas2020
+ejabberdctl register opensrf private.localhost xmas2020
+ejabberdctl register router public.localhost xmas2020
+ejabberdctl register opensrf public.localhost xmas2020
